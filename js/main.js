4bae348b729f95e1f22efded72e41dd67e57891a
currycurry <curry@alum.mit.edu>
2017-08-30 23:55:32 -0700

    redone with d3

--- js/main.js
+++ js/main.js
@@ -1,128 +1,157 @@
-var boundingWidth;
-var boundingHeight;
-var windowWidth;
-var windowHeight;
+var width = Math.max(960, innerWidth),
+    height = Math.max(500, innerHeight);
 
-function resizeAndRedrawCanvas()
-{
-  var desiredWidth = $(window).width(); // For instance: $(window).width();
-  var desiredHeight = $(window).height(); // For instance $('#canvasContainer').height();
-
-  boundingWidth = desiredWidth/3;
-  boundingHeight = boundingWidth;
-  windowWidth = desiredWidth;
-  windowHeight = desiredHeight;
-
-  view.viewSize = new Size(desiredWidth, desiredHeight);
-  view.draw();
-}
-
-paper.install(window);
-
-var background;
-$( document ).ready(function() {
-  resizeAndRedrawCanvas();
-  var backgroundRect = new Rectangle(new Point(0, 0), view.size);
-  background = new Path.Rectangle(backgroundRect);
-  background.fillColor = {
-  	gradient: {
-  		stops: [ ['white', 0], ['darkBlue', 1] ]
-  	},
-  	origin: [ windowWidth / 2, 0 ],
-  	destination: [ windowWidth / 2, windowHeight ]
-  }
-
-  background.sendToBack();
-
-  initContainers();
-
-});
-
-var sketch_container;
-var matrix_container;
+var margin = 10;
+var container_width = width / 2 - 2 * margin;
+var container_height = container_width * .75;
 var matrix;
-var offColor = 'black';
+var offColor = '#686868';
 var onColor = 'white';
+var bPressed = false;
 var num_hits = 16;
 var num_notes = 10;
-matrix_cell_rectangle = new Array();
-matrix = new Array();
-cell_color = new Array();
-pressed = new Array();
+var cell_margin = 2;
+var cell_width = ( container_width - cell_margin * ( num_hits + 1 ) ) / num_hits;
+var cell_height =  ( container_height - cell_margin * ( num_notes + 1 ) ) / num_notes;
+var bDrawing = false;
 
-function initContainers() {
-	  //draw 
-  var margin = 10;
-  var cell_margin = 2;
-  var container_width = ( windowWidth - 3 * margin ) / 2 ;
-  var container_height = container_width * .75;
-  var container_size = new Size( container_width, container_height );
-  var sketch_rectangle = new Rectangle(new Point( margin, margin ), container_size );
-  sketch_container = new Path.Rectangle(sketch_rectangle);
-  sketch_container.fillColor = '#484848';
+var matrixData = matrixData();
+//console.log( matrixData );
 
-  var matrix_rectangle = new Rectangle(new Point( (windowWidth + margin ) / 2, margin ), container_size );
-  matrix_container = new Path.Rectangle(matrix_rectangle);
-  matrix_container.fillColor = '#484848';
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
 
-  var cell_size = new Size(( matrix_rectangle.width - cell_margin * ( num_hits - 1 ) ) / num_hits, ( matrix_rectangle.height - cell_margin * ( num_notes - 1 ) ) / num_notes );
+var sketch_container = d3.select("svg").append("svg")
+  .attr("width", width / 2)
+  .attr("height", width / 2 * .75 );
 
-  for ( var i = 0; i < num_hits; i ++ ) {
-  	matrix_cell_rectangle[ i ] = new Array();
-  	matrix[ i ] = new Array();
-  	cell_color[ i ] = new Array();
-  	pressed[ i ] = new Array();
-  	for ( var j = 0; j < num_notes; j ++ ) {
-  		matrix_cell_rectangle[ i ][ j ] = new Rectangle( new Point( matrix_rectangle.x + i * ( cell_size.width + cell_margin ), matrix_rectangle.y + j * ( cell_size.height + cell_margin ) ), cell_size );
-  		matrix[ i ][ j ] = new Path.Rectangle( matrix_cell_rectangle[ i ][ j ] );
-  		cell_color[ i ][ j ] = offColor;
-  		matrix[ i ][ j ].fillColor = cell_color[ i ][ j ];
+var matrix_container = d3.select("svg").append("svg")
+  .attr("width", width / 2)
+  .attr("height", width / 2 * .75 )
+  .attr("x", width / 2 );
 
-  		pressed[ i ][ j ] = false;
-  	}
+sketch_container.append("rect")
+    .attr("width", container_width)
+    .attr("height", container_height)
+    .attr("x", margin)
+    .attr("y", margin)
+    .on("mousedown", handleStartDrawing)
+    .on("mousemove", sketch )
+    .on("mouseup", handleEndDrawing )
+    .on("mouseout", handleEndDrawing );
+
+matrix_container.append("rect")
+    .attr("width", container_width)
+    .attr("height", container_height)
+    .attr("x", margin)
+    .attr("y", margin);
+
+function matrixData() {
+    var matrix = new Array();
+    var xpos = margin + cell_margin; //starting xpos and ypos 
+    var ypos = margin + cell_margin;
+    var pressed = false;
+
+    // iterate for rows 
+    for (var row = 0; row < num_notes; row++) {
+        matrix.push( new Array() );
+
+        // iterate for cells/columns inside rows
+        for (var column = 0; column < num_hits; column++) {
+            matrix[row].push({
+                x: xpos,
+                y: ypos,
+                width: cell_width,
+                height: cell_height,
+                pressed: pressed
+            })
+            // increment the x position
+            xpos += cell_width + cell_margin;
+        }
+        // reset the x position after a row is complete
+        xpos = margin + cell_margin;
+        // increment the y position for the next row. Move it down 50 (height variable)
+        ypos += cell_height + cell_margin; 
+    }
+    return matrix;
+}
+  
+var row = matrix_container.selectAll(".row")
+  .data(matrixData)
+  .enter().append("g")
+  .attr("class", "row");
+  
+var column = row.selectAll(".cell")
+  .data(function(d) { return d; })
+  .enter().append("rect")
+  .attr("class","cell")
+  .attr("x", function(d) { return d.x; })
+  .attr("y", function(d) { return d.y; })
+  .attr("width", function(d) { return d.width; })
+  .attr("height", function(d) { return d.height; })
+  .style("fill", offColor )
+  .style("stroke", offColor )
+  .on('click', function(d) {
+     d.pressed = !d.pressed;
+     if ( d.pressed ) {
+        d3.select(this).style("fill", onColor );
+        d3.select(this).style("stroke", onColor );
+     }
+     else {
+        d3.select(this).style("fill", offColor );
+        d3.select(this).style("stroke", offColor );
+     }
+    });
+
+// active line drawing
+var ptdata = [];
+var session = [];
+var path;
+var line;
+
+function handleStartDrawing() {
+  bDrawing = true;
+  ptdata = [];
+  line = d3.line()
+    .x(function(d, i) { return d.x; })
+    .y(function(d, i) { return d.y; });
+
+  path = sketch_container.append("path") // start a new line
+    .data([ptdata])
+    .attr("class", "line")
+    .attr("d", line);
+}
+
+function sketch() {
+  if ( bDrawing ) {
+    var point = d3.mouse(this);
+    ptdata.push({ x: point[0], y: point[1] });
+    path.attr("d", function(d) { return line(d); }) // Redraw the path:
   }
-
 }
 
-function onMouseMove(event) {
-
-
+function handleEndDrawing() {
+  if ( bDrawing ){
+    bDrawing = false;
+    ptdata = simplify(ptdata);
+    session.push(ptdata);
+  } 
 }
 
-function onMouseDown(event) {
+// initialize the gui
+var params = {
+  bars : 4,
+  erase : false,
+  loop : true
+};
 
-	for ( var i = 0; i < num_hits; i ++ ) {
-	    for( var j = 0; j < num_notes; j ++ ) {
-	        
-	        if( matrix[ i ][ j ].bounds.contains( event.point )) {
-	            
-	            pressed[ i ][ j ] = !pressed[ i ][ j ];
-
-	            
-	            if ( pressed[ i ][ j ] ) {
-	                cell_color[ i ][ j ] = onColor;
-
-	            }
-	            
-	            if ( !pressed[ i ][ j ] ) {
-	                //if ( i % hits_per_beat ==0 ) {
-	                  //  box_color[ i ][ j ] = downbeat_off_color;
-	                //}
-	                //else {
-	                    cell_color[ i ][ j ] = offColor;
-	                //}
-	            }
-	            matrix[ i ][ j ].fillColor = cell_color[ i ][ j ];
-
-	        }
-	    }
-	}
-
+function initGui() {
+  var gui = new dat.GUI();
+  gui.remember(params);
+  gui.add(params, 'bars', 1, 8).listen();
+  gui.add(params, 'erase');
+  gui.add(params, 'loop');
 }
 
-function onMouseUp(event) {
-
-
-}
-
-
+initGui();
