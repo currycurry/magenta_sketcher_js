d03554fa73d5f136eac2ca72c827fd47e955f9a1
currycurry <curry@alum.mit.edu>
2017-09-03 23:02:25 -0700

    erase working better, contained within rectangle

--- js/main.js
+++ js/main.js
@@ -25,7 +25,11 @@ var svg = d3.select("body").append("svg")
 
 var sketch_container = d3.select("svg").append("svg")
   .attr("width", width / 2)
-  .attr("height", width / 2 * .75 );
+  .attr("height", width / 2 * .75 )    
+  .on("mousedown", handleStartDrawing )
+    .on("mousemove", sketch )
+    .on("mouseup", handleEndDrawingUp )
+    .on("mouseleave", handleEndDrawingOut );
 
 var matrix_container = d3.select("svg").append("svg")
   .attr("width", width / 2)
@@ -36,11 +40,7 @@ sketch_container.append("rect")
     .attr("width", container_width)
     .attr("height", container_height)
     .attr("x", margin)
-    .attr("y", margin)
-    .on("mousedown", handleStartDrawing)
-    .on("mousemove", sketch )
-    .on("mouseup", handleEndDrawing )
-    .on("mouseout", handleEndDrawing );
+    .attr("y", margin);
 
 matrix_container.append("rect")
     .attr("width", container_width)
@@ -113,30 +113,80 @@ var line;
 
 function handleStartDrawing() {
   bDrawing = true;
-  ptdata = [];
-  line = d3.line()
-    .x(function(d, i) { return d.x; })
-    .y(function(d, i) { return d.y; });
+  if ( !params.erase ) {
+    ptdata = [];
+    line = d3.line()
+      .x(function(d, i) { return d.x; })
+      .y(function(d, i) { return d.y; });
 
-  path = sketch_container.append("path") // start a new line
-    .data([ptdata])
-    .attr("class", "line")
-    .attr("d", line);
+    path = sketch_container.append("path") // start a new line
+      .data([ptdata])
+      .attr("class", "line")
+      .attr("d", line);
+  }
+
 }
 
 function sketch() {
+  //console.log("bdrawing: " + bDrawing);
   if ( bDrawing ) {
     var point = d3.mouse(this);
-    ptdata.push({ x: point[0], y: point[1] });
-    path.attr("d", function(d) { return line(d); }) // Redraw the path:
+
+    if ( params.erase ) {
+      console.log( "erasing: " + point );
+      erase( point );
+    }
+
+    else {
+      //console.log( "drawing: " + point );
+      ptdata.push({ x: point[0], y: point[1] });
+      path.attr("d", function(d) { return line(d); }) // Redraw the path:
+    }
   }
 }
 
-function handleEndDrawing() {
+var erase_radius = 15;
+
+function erase( m_pos ) {
+  console.log( "in erase function: " + m_pos );
+  if ( m_pos[ 0 ] > margin + erase_radius && m_pos[ 0 ] < margin + container_width - erase_radius ) {
+    if ( m_pos[ 1 ] > margin + erase_radius && m_pos[ 1 ] < margin + container_height - erase_radius ) {
+      sketch_container.insert('circle')
+      .attr("cx", m_pos[0])
+      .attr("cy", m_pos[1])
+      .attr("r", erase_radius)
+      .style("stroke", '#282828')
+      .style("fill", '#282828')
+      .style("stroke-opacity", 1);
+    }
+  }
+
+  //bDrawing = true;
+}
+
+function handleEndDrawingUp() {
+  console.log("endDrawing up called");
   if ( bDrawing ){
     bDrawing = false;
-    ptdata = simplify(ptdata);
-    session.push(ptdata);
+    if ( !params.erase ){
+      ptdata = simplify(ptdata);
+      session.push(ptdata);
+
+    }
+
+  } 
+}
+
+function handleEndDrawingOut() {
+  console.log("endDrawing out called");
+  if ( bDrawing ){
+    bDrawing = false;
+    if ( !params.erase ){
+      ptdata = simplify(ptdata);
+      session.push(ptdata);
+
+    }
+
   } 
 }
 
@@ -150,13 +200,30 @@ var params = {
   num_notes : 10
 };
 
+var clearCallback;
+
 function initGui() {
   var gui = new dat.GUI( );
   //gui.remember(params);
   gui.add(params, 'bars', 1, 8).listen();
   gui.add(params, 'erase');
   gui.add(params, 'save');
-  gui.add(params, 'clear');
+  gui.add(params, 'clear').onChange(function(newValue) {
+    console.log("clear:  ", newValue);
+    if ( newValue == true ) {
+      sketch_container.selectAll("*").remove();
+      sketch_container.append("rect")
+      .attr("width", container_width)
+      .attr("height", container_height)
+      .attr("x", margin)
+      .attr("y", margin)
+      .on("mousedown", handleStartDrawing)
+      .on("mousemove", sketch )
+      .on("mouseup", handleEndDrawingUp )
+      .on("mouseleave", handleEndDrawingOut );
+      params.clear = false;
+    }
+  });
   gui.add(params, 'num_hits', 4, 128);
   gui.add(params, 'num_notes', 5, 40);
 
